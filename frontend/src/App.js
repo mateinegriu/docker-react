72632b6039fbf8c6879610d8eba985f0cdeb5e8a
Matei Negriu <Matei.Negriu@viavisolutions.com>
2021-05-10 18:07:11 +0300

    travis: trying to make it work again
    Signed-off-by: Matei Negriu <Matei.Negriu@viavisolutions.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react with matei negriu
+          last time's a charm
         </a>
       </header>
     </div>
